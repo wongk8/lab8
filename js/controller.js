cdf9985457bab8cb8b330413581a0be8d1fc3829
Mikey Wong <wongk8@uw.edu>
2013-11-18 00:37:29 -0800

    Added Optional Steps

--- js/controller.js
+++ js/controller.js
@@ -4,5 +4,74 @@
 
 $(function(){
 
+	var formatLabels = {
+	    dvd: 'DVD',
+	    bluray: 'Blu-Ray'
+	};
+
+	var cartModel = createCartModel();
+
+	var cartView = createCartView({
+	    model: cartModel,
+	    template: $('.cart-item-template'),
+	    container: $('.cart-items-container'),
+	    totalPrice: $('.total-price')
+	});
+
+	var moviesModel = createMoviesModel({
+	    url: 'https://courses.washington.edu/info343/ajax/movies/'
+	});
+
+	var cartJSON = localStorage.getItem('cart');
+	if (cartJSON && cartJSON.length > 0) {
+	    cartModel.setItems(JSON.parse(cartJSON));
+	}
+
+	var moviesView = createMoviesView({
+	    model: moviesModel,
+	    template: $('.movie-template'),
+	    container: $('.movies-container')
+	});
+
+	//refresh to get movies from server
+	moviesModel.refresh();
+
+	//when the movies view triggers 'addToCart'
+	//add a new item to the cart, using the supplied
+	//movieID and format
+	moviesView.on('addToCart', function(data){
+	    var movie = moviesModel.getItem(data.movieID);
+	    if (!movie)
+	        throw 'Invalid movie ID "' + movieID + '"!'; 
+
+	    cartModel.addItem({
+	        id: movie.id,
+	        title: movie.title,
+	        format: data.format,
+	        formatLabel: formatLabels[data.format],
+	        price: movie.prices[data.format]
+	    });
+	}); //addToCart event
+
+    $('.place-order').click(function(){
+        $.ajax({
+            url: 'https://courses.washington.edu/info343/ajax/movies/orders/',
+            type: 'POST',
+            data: cartModel.toJSON(),
+            contentType: 'application/json',
+            success: function(responseData) {
+                alert(responseData.message);
+                    cartModel.setItems([]);
+            },
+            error: function(jqXHR, status, errorThrown) {
+                //error with post--alert user
+                alert(errorThrown || status);
+            }
+        }); //ajax() 
+    });
+
+	cartModel.on('change', function(){
+	    localStorage.setItem('cart', cartModel.toJSON());
+	});
 }); //doc ready()
 
